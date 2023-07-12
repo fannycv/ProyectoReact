904d343168b4621e0788925d7d8d329a2337f5e4
Estefany Castro <estefanycv74@gmail.com>
2023-07-12 11:18:27 -0500

    actualizacion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://apiloginproyectointegrador.onrender.com',
 });
 
 export function Login() {
